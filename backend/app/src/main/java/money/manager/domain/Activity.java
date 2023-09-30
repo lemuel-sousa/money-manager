e3a14487bf30a15dcf4535eb9304c97b98480f3c
lemuel-sousa <lemuel124.ldsp@gmail.como>
2023-09-29 23:26:14 -0300

    feat: create Activity domain

---
+++ backend/app/src/main/java/money/manager/domain/Activity.java
@@ -0,0 +1,137 @@
+package money.manager.domain;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import money.manager.domain.exception.DomainException;
+import money.manager.domain.type.ActivityType;
+import money.manager.utils.InstantUtils;
+
+public class Activity {
+
+  private String id;
+  private String description;
+  private Instant date;
+  private float value;
+  private ActivityType type;
+  private Instant createdAt;
+  private Instant updatedAt;
+
+  private Activity(final String anId, final String aDescription, final Instant aDate, final float aValue,
+      final ActivityType aType, final Instant aCreatedAt, final Instant anUpdatedAt) {
+
+    this.id = anId;
+    this.description = aDescription;
+    this.date = aDate;
+    this.value = aValue;
+    this.type = aType;
+    this.createdAt = aCreatedAt;
+    this.updatedAt = anUpdatedAt;
+
+    this.validate();
+  }
+
+  public static Activity newActivity(final String aDescription, final Instant aDate,
+      final float aValue, final ActivityType aType) {
+
+    return new Activity(
+        UUID.randomUUID().toString().toLowerCase(),
+        aDescription,
+        InstantUtils.now(),
+        aValue,
+        aType,
+        InstantUtils.now(),
+        InstantUtils.now());
+  }
+
+  public static Activity with(final String anId, final String aDescription, final Instant aDate,
+      final float aValue, final ActivityType aType, final Instant aCreatedAt, final Instant anUpdatedAt) {
+    return new Activity(
+        anId,
+        aDescription,
+        aDate,
+        aValue,
+        aType,
+        aCreatedAt,
+        anUpdatedAt);
+  }
+
+  private void validate() {
+
+    if (this.id.isBlank()) {
+      throw new DomainException("Activity's ID should not be blank.");
+    }
+    if (this.id.length() != 36) {
+      throw new DomainException("Activity's should be a valid UUID.");
+    }
+    if (this.description.isBlank()) {
+      throw new DomainException("Activity's description should not be blank.");
+    }
+    if (this.description.length() < 3) {
+      throw new DomainException("Activity's description should have more than 3 characters.");
+    }
+    if (this.type != ActivityType.REVENUE && this.type != ActivityType.EXPENSE) {
+      throw new DomainException("Activity's type should be revenue or expense.");
+    }
+    if (this.value < 0.01) {
+      throw new DomainException("Activity's value should be greater than zero.");
+    }
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Instant getDate() {
+    return date;
+  }
+
+  public void setDate(Instant date) {
+    this.date = date;
+  }
+
+  public float getValue() {
+    return value;
+  }
+
+  public void setValue(float value) {
+    this.value = value;
+  }
+
+  public ActivityType getType() {
+    return type;
+  }
+
+  public void setType(ActivityType type) {
+    this.type = type;
+  }
+
+  public Instant getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Instant createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Instant getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(Instant updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+}
