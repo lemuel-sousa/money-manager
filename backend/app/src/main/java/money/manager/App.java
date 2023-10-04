9d035f4e97ff3fcefa1be7a610a4d806e922f1c3
lemuel-sousa <lemuel124.ldsp@gmail.como>
2023-10-03 22:41:46 -0300

    feat: configure spring web server

--- backend/app/src/main/java/money/manager/App.java
+++ backend/app/src/main/java/money/manager/App.java
@@ -3,12 +3,24 @@
  */
 package money.manager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import money.manager.config.WebserverConfiguration;
+
+@SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        LOG.info("Starting money.manager...");
+
+        SpringApplication.run(WebserverConfiguration.class, args);
+
+        LOG.info("Ending of starting money.manager...");
+
     }
 }
