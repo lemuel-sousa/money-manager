84f193237637ddcfc0f820bf5b181296934675c9
lemuel-sousa <lemuel124.ldsp@gmail.como>
2023-10-04 16:58:26 -0300

    feat: update validations

--- backend/app/src/main/java/money/manager/api/controller/activity/dto/InsertActivityRequestDto.java
+++ backend/app/src/main/java/money/manager/api/controller/activity/dto/InsertActivityRequestDto.java
@@ -5,8 +5,8 @@ import java.time.Instant;
 import jakarta.validation.constraints.NotBlank;
 
 public record InsertActivityRequestDto(
-  @NotBlank(message = "descpription field is mandatory.") String description,
-  @NotBlank(message = "date field is mandatory.") Instant date,
-  @NotBlank(message = "value field is mandatory.") float value,
-  @NotBlank(message = "type field is mandatory.") String type) {
+    @NotBlank(message = "descpription field is mandatory.") String description,
+    Instant date,
+    float value,
+    @NotBlank(message = "type field is mandatory.") String type) {
 }
