a72e1913c083dbd8500480a670ceba280d358d37
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-11-07 18:53:16 -0300

    feat: create recover validation service

--- backend/app/src/main/java/money/manager/service/auth/TokenService.java
+++ backend/app/src/main/java/money/manager/service/auth/TokenService.java
@@ -5,8 +5,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import money.manager.service.auth.dto.input.TokenServiceInputDto;
 import money.manager.service.auth.dto.output.TokenServiceOutputDto;
 import money.manager.service.auth.dto.output.TokenSubjectServiceOutputDto;
+import money.manager.service.auth.dto.output.ValidateTokenServiceOutputDto;
 
 public interface TokenService {
     TokenServiceOutputDto createToken(final UserDetails aUser);
     TokenSubjectServiceOutputDto validateToken(final TokenServiceInputDto anInput);
+    ValidateTokenServiceOutputDto recoverValidation(final TokenServiceInputDto anInput);
+    
 }
--- backend/app/src/main/java/money/manager/service/auth/implementation/TokenServiceImpl.java
+++ backend/app/src/main/java/money/manager/service/auth/implementation/TokenServiceImpl.java
@@ -5,32 +5,26 @@ import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.AlgorithmMismatchException;
-import com.auth0.jwt.exceptions.IncorrectClaimException;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.exceptions.MissingClaimException;
-import com.auth0.jwt.exceptions.SignatureVerificationException;
-import com.auth0.jwt.exceptions.TokenExpiredException;
 
 import money.manager.service.auth.TokenService;
 import money.manager.service.auth.dto.input.TokenServiceInputDto;
 import money.manager.service.auth.dto.output.TokenServiceOutputDto;
 import money.manager.service.auth.dto.output.TokenSubjectServiceOutputDto;
+import money.manager.service.auth.dto.output.ValidateTokenServiceOutputDto;
 import money.manager.service.auth.exception.AuthenticationException;
 import money.manager.utils.InstantUtils;
 
 @Service
-public class TokenServiceImpl implements TokenService{
+public class TokenServiceImpl implements TokenService {
 
     private final String TOKEN_SECRET = "money-manager-security-token-secret";
     private final String TOKEN_ISSUER = "money.manager";
 
-    public static TokenServiceImpl build(){
+    public static TokenServiceImpl build() {
         return new TokenServiceImpl();
     }
 
-    
     public TokenServiceOutputDto createToken(final UserDetails aUser) {
         try {
             final var anAlgorithm = Algorithm.HMAC256(TOKEN_SECRET);
@@ -39,7 +33,7 @@ public class TokenServiceImpl implements TokenService{
                     .withSubject(aUser.getUsername())
                     .withExpiresAt(InstantUtils.now().plusSeconds(60 * 60 * 4)) // plus 4 hours
                     .sign(anAlgorithm);
-            
+
             return new TokenServiceOutputDto(aToken);
         } catch (JWTCreationException e) {
             throw new AuthenticationException(e.getMessage());
@@ -48,9 +42,9 @@ public class TokenServiceImpl implements TokenService{
         }
     }
 
+    @Override
     public TokenSubjectServiceOutputDto validateToken(final TokenServiceInputDto input) {
         try {
-            System.out.println(TOKEN_SECRET);
             final var anAlgorithm = Algorithm.HMAC256(TOKEN_SECRET);
             final var aVerifier = JWT.require(anAlgorithm)
                     .withIssuer(TOKEN_ISSUER)
@@ -61,21 +55,23 @@ public class TokenServiceImpl implements TokenService{
 
             return new TokenSubjectServiceOutputDto(aTokenSubject);
 
-        } catch (IllegalArgumentException e) {
-            throw new AuthenticationException(e.getMessage());
-        } catch (AlgorithmMismatchException e) {
-            throw new AuthenticationException(e.getMessage());
-        } catch (SignatureVerificationException e) {
-            throw new AuthenticationException(e.getMessage());
-        } catch (TokenExpiredException e) {
-            throw new AuthenticationException(e.getMessage());
-        } catch (MissingClaimException e) {
-            throw new AuthenticationException(e.getMessage());
-        } catch (IncorrectClaimException e) {
-            throw new AuthenticationException(e.getMessage());
-        } catch (JWTVerificationException e) {
-            throw new AuthenticationException(e.getMessage());
+        } catch (Exception e) {
+            // throw new ValidateException(e.getMessage());
+            return new TokenSubjectServiceOutputDto("");
         }
+
+    }
+
+    @Override
+    public ValidateTokenServiceOutputDto recoverValidation(final TokenServiceInputDto aInput) {
+        final var aTokenSubject = this.validateToken(aInput);
+
+        final var isValid = new ValidateTokenServiceOutputDto(false);
+        
+        if (!aTokenSubject.subject().isBlank()) 
+            return new ValidateTokenServiceOutputDto(true);
+
+        return isValid; 
     }
 
 }
